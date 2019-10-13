4264df4c3dd891ba8f40e2758847254d8284d402
shura <alexandra_shmoylova@yahoo.com>
2019-10-13 12:01:16 +0400

    map

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,11 @@
 var popup = document.querySelector('.modal-wrapper');
 var popupButton = document.querySelector('.modal-btn-search');
-var form = popup.querySelector('form');
-var arrive = popup.querySelector('[name=arrive]');
-var depart = popup.querySelector('[name=depart]');
 
 popup.classList.add('hidden');
 
-popupButton.addEventListener('click', function(evt) {
-	evt.preventDefault();
-  	popup.classList.toggle('hidden');
-  	arrive.focus();
+popupButton.addEventListener('click', function() {
+	popup.classList.toggle('hidden');
 });
-
-form.addEventListener('submit', function (evt) {
-	if (!arrive.value || !depart.value) {
-      evt.preventDefault();
-      console.log("Нужно ввести дату заезда и выезда");
-  }  else {
-      localStorage.setItem("arrive", arrive.value);
-      localStorage.setItem("depart", depart.value);
-  }
-  });
 
 
 
